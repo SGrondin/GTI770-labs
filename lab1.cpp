60eaff89dda128c2c49595e4af7335b1420dfa8e
Olivier Arteau <arteau.olivier@gmail.com>
2015-05-08 10:44:39 -0400

    Added answer

--- lab1.cpp
+++ lab1.cpp
@@ -18,7 +18,7 @@
 
 // DEFINES
 #define NUM_SAMPLES 100
-#define NUM_FEATURES 5
+#define NUM_FEATURES 4
 
 // Bart Train: 80 items: bart1.bmp - bart80.bmp
 // Homer Train 62 items: homer1.bmp - homer62.bmp
@@ -47,10 +47,6 @@ int main( int argc, char** argv )
 	// Feature vector [rows] [columns]
 	// In fact it is a "matrix of features"
 	float fVector[ NUM_SAMPLES ][ NUM_FEATURES ];
-
-	// Feature variables
-	float fOrange;
-	float fWhite;
 
 	// Variable filename
 	static char cFileName[ 50 ] = {'\0'};
@@ -103,10 +99,17 @@ int main( int argc, char** argv )
 
 	// Take all the image files at the range
 
+	float numberPixelBeardHomer;
+	float numberPixelBlueHomer;
+	float numberPixelOrangeBart;
+	float numberPixelBlueBart;
+
 	for (ii =0; ii<2; ii++)
 	{
-		for ( iNum = 1; iNum <= 62; iNum++ )
+		for ( iNum = 1; iNum <= 80; iNum++ )
 		{
+
+
 			// Build the image filename and path to read from disk
 			sprintf ( cFileName, path[ii], (int)(iNum) );
 			printf ( " %s\n", cFileName);
@@ -120,7 +123,6 @@ int main( int argc, char** argv )
 
 			// Gets the image size (width, height) 'img'
 			if (img == NULL) {
-				printf("Unable to load image '%s' \n", cFileName);
 				continue;
 			}
 
@@ -137,8 +139,10 @@ int main( int argc, char** argv )
 			threshold  	= cvCloneImage( img );
 
 			// Initialize variables with zero
-			fOrange 	= 0.0;
-			fWhite 	= 0.0;
+			numberPixelBeardHomer = 0.0;
+			numberPixelBlueHomer = 0.0;
+			numberPixelOrangeBart = 0.0;
+			numberPixelBlueBart = 0.0;
 
 			// Loop that reads each image pixel
 			for( h = 0; h < img->height; h++ ) // rows
@@ -159,45 +163,50 @@ int main( int argc, char** argv )
 					// Verify if the pixels have a given value ( Orange, defined as R[240-255], G[85-105], B[11-22] ). If so, count it...
 					if ( blue>=11 && blue<=22 && green>=85 && green<=105 &&  red>=240 && red<=255 )
 					{
-						fOrange++;
-
-						// Just to be sure we are doing the right thing, we change the color of the orange pixels to green [R=0, G=255, B=0] and show them into a cloned image (processed)
-
-						( (uchar *)(processed->imageData + h*processed->widthStep) )[ w*processed->nChannels + 0 ] = 0;
-						( (uchar *)(processed->imageData + h*processed->widthStep) )[ w*processed->nChannels + 1 ] = 255;
-						( (uchar *)(processed->imageData + h*processed->widthStep) )[ w*processed->nChannels + 2 ] = 0;
+						numberPixelOrangeBart++;
 					}
 
-					// Detect and count the number of white pixels (just a dummy feature...)
-					// Verify if the pixels have a given value ( White, defined as R[253-255], G[253-255], B[253-255] ). If so, count it...
-					if ( blue >= 253 && green >= 253 && red >= 253 )
+					if ( blue>=122 && blue<=142 && green>=0 && green<=18 &&  red>=0 && red<=10 )
 					{
-						fWhite++;
+						numberPixelBlueBart++;
 					}
 
-					// Here you can add your own features....... Good luck
+					if ( blue>=97 && blue<=117 && green>=163 && green<=183 &&  red>=179 && red<=199 )
+					{
+						numberPixelBeardHomer++;
+					}
 
+					if ( blue>=163 && blue<=183 && green>=97 && green<=117 &&  red>=0 && red<=10 )
+					{
+						numberPixelBlueHomer++;
+					}
 				}
 			}
 
 			// Lets make our counting somewhat independent on the image size...
 			// Compute the percentage of pixels of a given colour.
 			// Normalize the feature by the image size
-			fOrange 	= fOrange / ( (int)img->height * (int)img->width );
-			fWhite  	= fWhite  / ( (int)img->height * (int)img->width );
+			numberPixelOrangeBart 	= numberPixelOrangeBart / ( (int)img->height * (int)img->width );
+			numberPixelBlueBart  	= numberPixelBlueBart  / ( (int)img->height * (int)img->width );
+			numberPixelBeardHomer  	= numberPixelBeardHomer  / ( (int)img->height * (int)img->width );
+			numberPixelBlueHomer  	= numberPixelBlueHomer  / ( (int)img->height * (int)img->width );
 
 			// Store the feature value in the columns of the feature (matrix) vector
-			fVector[iNum][1] = fOrange;
-			fVector[iNum][2] = fWhite;
+			fVector[iNum][1] = numberPixelOrangeBart;
+			fVector[iNum][2] = numberPixelBlueBart;
+			fVector[iNum][3] = numberPixelBeardHomer;
+			fVector[iNum][4] = numberPixelBlueHomer;
 
 			// Here you can add more features to your feature vector by filling the other columns: fVector[iNum][3] = ???; fVector[iNum][4] = ???;
 
 			// Shows the feature vector at the screen
-			printf( "\n%d %f %f", iNum, fVector[iNum][1], fVector[iNum][2] );
+			printf( "\n%d %f %f %f %f", iNum, fVector[iNum][1], fVector[iNum][2], fVector[iNum][3], fVector[iNum][4]);
 
 			// And finally, store your features in a file
 			fprintf( fp, "%f,", fVector[iNum][1]);
 			fprintf( fp, "%f,", fVector[iNum][2]);
+			fprintf( fp, "%f,", fVector[iNum][3]);
+			fprintf( fp, "%f,", fVector[iNum][4]);
 
 			// IMPORTANT
 			// Do not forget the label....
